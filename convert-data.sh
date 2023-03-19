dd97c5728c722cddc9bba5bca9fd436fa2f516de
Manuele Nolli <manuele.nolli@hotmail.it>
2023-03-19 09:01:05 +0100

    Added script

---
+++ convert-data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Accepts an argument that specifies the path of the csv file to be used
+# If no argument is provided, notify the user
+if [ $# -eq 0 ]; then
+    echo "[!] Please provide the path of the csv file to be used!"
+    exit 1
+fi
+
+# If the argument is provided, check if the file exists
+if [ ! -f "$1" ]; then
+    echo "[!] CSV file does not exist"
+    exit 1
+fi
+
+# Then, run the executable
+./cmake-build-debug/converter -i "$1"
