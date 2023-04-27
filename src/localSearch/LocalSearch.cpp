6898e164f943c41abeecd8cc3793efa2bd3e2d4b
mattiaDellOca <mattia.delloca@student.supsi.ch>
2023-04-27 09:26:47 +0200

    Code clean-up

--- src/localSearch/LocalSearch.cpp
+++ src/localSearch/LocalSearch.cpp
@@ -2,55 +2,53 @@
 // Created by mattia on 20/04/23.
 //
 #include "LocalSearch.h"
-#include <iostream>
 
+/**
+ * Compute the local search algorithm for the Multi-Objective Multi-Dimensional Knapsack Problem
+ * Interrupt the algorithm when a SIGINT is received
+ * @param instance The instance of the Multi-Objective Multi-Dimensional Knapsack Problem
+ */
 void LocalSearch::compute(Data *instance) {
-    std::cout << "----------------- Local search solution -----------------" << std::endl;
+    // Initialize random seed
     srand(time(nullptr));
 
-    for (long unsigned int i = 0; i < instance->capacities.size(); i++) {
-        std::cout << instance->capacities[i] << "_";
-    }
-    std::cout << std::endl;
-
+    // Continue until stop condition is reached
     while (!stopCondition()) {
-        std::vector<int> neighborhood = computeNeighborhood(instance);
+        std::vector<int> neighbour = computeNeighbour(instance);
 
         // Update capacities
-        std::vector<int> neighborhoodCapacities = instance->capacities;
-        for (long unsigned int i = 0; i < neighborhood.size(); i++) {
-            if (neighborhood[i] != instance->solution[i]) {
+        std::vector<int> neighborCapacities = instance->capacities;
+        for (long unsigned int i = 0; i < neighbour.size(); i++) {
+            // Update only classes that have changed
+            if (neighbour[i] != instance->solution[i]) {
                 for (int j = 0; j < instance->nresources; j++) {
-                    neighborhoodCapacities[j] -= instance->weights[i][neighborhood[i] * instance->nresources + j];
-                    neighborhoodCapacities[j] += instance->weights[i][instance->solution[i] * instance->nresources + j];
+                    neighborCapacities[j] -= instance->weights[i][neighbour[i] * instance->nresources + j];
+                    neighborCapacities[j] += instance->weights[i][instance->solution[i] * instance->nresources + j];
                 }
             }
         }
 
         // Update solution
-        instance->solution = neighborhood;
-        instance->capacities = neighborhoodCapacities;
-        int totValue = 0;
-        for (long unsigned int i = 0; i < instance->solution.size(); i++) {
-            totValue += instance->values[i][instance->solution[i]];
-        }
-        std::cout << "Solution new value: " << totValue << std::endl;
+        instance->solution = neighbour;
+        instance->capacities = neighborCapacities;
     }
-
-    for (unsigned long i = 0; i < instance->capacities.size(); i++)
-        std::cout << instance->capacities[i] << "_";
-    std::cout << std::endl;
 }
 
-int LocalSearch::m_iterations = 0;
-
+/**
+ * Check if the stop condition is reached
+ * @return Always return false
+ */
 bool LocalSearch::stopCondition() {
-    // Might change in the future
     return false;
 }
 
-std::vector<int> LocalSearch::computeNeighborhood(Data *instance) {
-    std::vector<int> neighborhood = instance->solution;
+/**
+ * Compute a neighbour of the current solution
+ * @param instance The instance of the Multi-Objective Multi-Dimensional Knapsack Problem
+ * @return A std::vector<int> containing the new solution
+ */
+std::vector<int> LocalSearch::computeNeighbour(Data *instance) {
+    std::vector<int> neighbour = instance->solution;
 
     // Choose two different target classes
     int firstTargetClass = rand() % instance->nclasses;
@@ -62,28 +60,28 @@ std::vector<int> LocalSearch::computeNeighborhood(Data *instance) {
     int itemFirstClass = rand() % instance->nitems[firstTargetClass];
     int itemSecondClass = rand() % instance->nitems[secondTargetClass];
 
-    std::vector<int> neighborhoodCapacities = instance->capacities;
+    std::vector<int> neighbourCapacities = instance->capacities;
     // Update capacities
     for (int j = 0; j < instance->nresources; j++) {
-        neighborhoodCapacities[j] -= instance->weights[firstTargetClass][itemFirstClass * instance->nresources + j];
-        neighborhoodCapacities[j] += instance->weights[firstTargetClass][instance->solution[firstTargetClass] * instance->nresources + j];
-        neighborhoodCapacities[j] -= instance->weights[secondTargetClass][itemSecondClass * instance->nresources + j];
-        neighborhoodCapacities[j] += instance->weights[secondTargetClass][instance->solution[secondTargetClass] * instance->nresources + j];
+        neighbourCapacities[j] -= instance->weights[firstTargetClass][itemFirstClass * instance->nresources + j];
+        neighbourCapacities[j] += instance->weights[firstTargetClass][instance->solution[firstTargetClass] * instance->nresources + j];
+        neighbourCapacities[j] -= instance->weights[secondTargetClass][itemSecondClass * instance->nresources + j];
+        neighbourCapacities[j] += instance->weights[secondTargetClass][instance->solution[secondTargetClass] * instance->nresources + j];
     }
-    // Check if neighborhood is feasible
+    // Check if neighbour is feasible
     bool feasible = true;
-    for (long unsigned int c = 0; c < neighborhoodCapacities.size(); c++) {
-        if (neighborhoodCapacities[c] < 0)
+    for (long unsigned int c = 0; c < neighbourCapacities.size(); c++) {
+        if (neighbourCapacities[c] < 0)
             feasible = false;
     }
 
-    // Update neighborhood is solution is sidible AND better
+    // Update neighbour is solution is feasible AND better
     if (feasible &&
                 instance->values[firstTargetClass][itemFirstClass] + instance->values[secondTargetClass][itemSecondClass] >
                 instance->values[firstTargetClass][instance->solution[firstTargetClass]] + instance->values[secondTargetClass][instance->solution[secondTargetClass]]) {
-            neighborhood[firstTargetClass] = itemFirstClass;
-            neighborhood[secondTargetClass] = itemSecondClass;
+        neighbour[firstTargetClass] = itemFirstClass;
+        neighbour[secondTargetClass] = itemSecondClass;
     }
 
-    return neighborhood;
+    return neighbour;
 }
