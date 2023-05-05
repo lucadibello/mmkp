8ee8534e72dd74db8ba54dc504c4d7ae6c238177
Luca Di Bello <info@lucadibello.ch>
2023-05-05 09:12:34 +0200

    Now public!

--- src/localSearch/LocalSearch.h
+++ src/localSearch/LocalSearch.h
@@ -12,10 +12,11 @@ class LocalSearch {
 public:
     static void compute(Data * instance);
 
+    static std::vector<int> computeNeighborhood(Data* instance);
+
 private:
     static int m_iterations;
     static bool stopCondition();
-    static std::vector<int> computeNeighborhood(Data* instance);
 };
 
 #endif //MMKP_LOCALSEARCH_H
